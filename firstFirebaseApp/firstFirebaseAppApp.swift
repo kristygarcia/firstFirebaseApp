dcce203785ec0e0de99e7f3fd93db6edbcb4ca51
Kristy Garcia <kristymichgarcia@gmail.com>
2023-09-17 19:54:33 -0400

    Initial Commit

---
+++ firstFirebaseApp/firstFirebaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstFirebaseAppApp.swift
+//  firstFirebaseApp
+//
+//  Created by Kristy Garcia on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct firstFirebaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
